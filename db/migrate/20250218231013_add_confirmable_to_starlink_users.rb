9a9f7412a4e562de888e93fbb60f06a5bc09c09d
geekelo <efueliht12@gmail.com>
2025-02-19 04:34:18 +0100

    FIx bugs

--- db/migrate/20250218231013_add_confirmable_to_starlink_users.rb
+++ db/migrate/20250218231013_add_confirmable_to_starlink_users.rb
@@ -3,9 +3,6 @@ class AddConfirmableToStarlinkUsers < ActiveRecord::Migration[7.1]
     add_column :starlink_users, :confirmation_token, :string
     add_column :starlink_users, :confirmation_sent_at, :datetime
     add_index :starlink_users, :confirmation_token, unique: true
-
-    # Ensure existing users are confirmed to avoid login issues
-    StarlinkUser.update_all(confirmed_at: Time.current)
   end
 
   def down
