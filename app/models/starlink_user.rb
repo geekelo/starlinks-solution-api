03ffc3cf89d89a51ccb737b61b791337712d601a
geekelo <efueliht12@gmail.com>
2025-02-19 23:53:59 +0100

    Ensure token gets saved

--- app/models/starlink_user.rb
+++ app/models/starlink_user.rb
@@ -1,7 +1,23 @@
 class StarlinkUser < ApplicationRecord
   has_secure_password
 
-  include Api::V1::EmailConfirmationsHelper
+  # include Api::V1::EmailConfirmationsHelper
   include Api::V1::PasswordResetsHelper
   include Api::V1::WhatsappConfirmationsHelper
+
+  def generate_confirmation_token
+    self.confirmation_token = SecureRandom.hex(20)
+    self.confirmation_sent_at = Time.current
+    save!
+    confirmation_token
+  end
+
+  def confirmation_token_valid?
+    confirmation_sent_at && confirmation_sent_at > 2.hours.ago
+  end
+
+  def confirm_email
+    update(email_confirmed: true, confirmation_token: nil, confirmation_sent_at: nil)
+  end
+
 end
