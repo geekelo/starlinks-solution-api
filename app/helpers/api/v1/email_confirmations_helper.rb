2e74f27b76476f15d9799ddc3187baebf7177e16
geekelo <efueliht12@gmail.com>
2025-02-20 01:16:58 +0100

    Ensure token gets saved

--- app/helpers/api/v1/email_confirmations_helper.rb
+++ app/helpers/api/v1/email_confirmations_helper.rb
@@ -1,5 +1,18 @@
 module Api::V1::EmailConfirmationsHelper
   extend ActiveSupport::Concern
 
-  
+  def generate_confirmation_token
+    self.confirmation_token = SecureRandom.hex(20)
+    self.confirmation_sent_at = Time.current
+    save!  # Save directly when called, but not before create
+    confirmation_token
+  end
+
+  def confirmation_token_valid?
+    confirmation_sent_at && confirmation_sent_at > 2.hours.ago
+  end
+
+  def confirm_email
+    update!(email_confirmed: true, confirmation_token: nil, confirmation_sent_at: nil)
+  end
 end
--- app/models/starlink_user.rb
+++ app/models/starlink_user.rb
@@ -1,20 +1,7 @@
 class StarlinkUser < ApplicationRecord
   include Api::V1::PasswordResetsHelper
   include Api::V1::WhatsappConfirmationsHelper
+  include Api::V1::EmailConfirmationsHelper
+  
   has_secure_password
-
-  def generate_confirmation_token
-    self.confirmation_token = SecureRandom.hex(20)
-    self.confirmation_sent_at = Time.current
-    save!  # Save directly when called, but not before create
-    confirmation_token
-  end
-
-  def confirmation_token_valid?
-    confirmation_sent_at && confirmation_sent_at > 2.hours.ago
-  end
-
-  def confirm_email
-    update!(email_confirmed: true, confirmation_token: nil, confirmation_sent_at: nil)
-  end
 end
