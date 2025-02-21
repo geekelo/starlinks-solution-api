7c8c7981aaa8693934554316fbb3a3e740407505
geekelo <efueliht12@gmail.com>
2025-02-21 04:08:43 +0100

    Fix bugs

--- app/controllers/api/v1/starlink_kits_controller.rb
+++ app/controllers/api/v1/starlink_kits_controller.rb
@@ -1,41 +1,42 @@
 class Api::V1::StarlinkKitsController < ApplicationController
-  before_action :set_starlink_kit, only: %i[show update destroy]
 
   def index
     if params[:user_id]
-      @starlink_kits = StarlinkKit.where(starlink_user_id: params[:user_id])
+      starlink_kits = StarlinkKit.where(starlink_user_id: params[:user_id])
     else
-      @starlink_kits = StarlinkKit.all
+      starlink_kits = StarlinkKit.all
     end
     
-    render json: @starlink_kits
+    render json: starlink_kits
   end
   
   def show
-    render json: @starlink_kit
+    render json: starlink_kit
   end
 
   def create
-    @starlink_kit = StarlinkKit.new(starlink_kit_params)
+    starlink_kit = StarlinkKit.new(starlink_kit_params)
   
-    if @starlink_kit.save
-      render json: @starlink_kit, status: :created
+    if starlink_kit.save
+      render json: starlink_kit, status: :created
     else
-      render json: @starlink_kit.errors, status: :unprocessable_entity
+      render json: starlink_kit.errors, status: :unprocessable_entity
     end
   end
   
 
   def update
-    if @starlink_kit.update(starlink_kit_params)
-      render json: @starlink_kit
+    starlink_kit = StarlinkKit.find(params[:id])
+    if starlink_kit.update(starlink_kit_params)
+      render json: starlink_kit
     else
-      render json: @starlink_kit.errors, status: :unprocessable_entity
+      render json: starlink_kit.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @starlink_kit.destroy
+    starlink_kit = StarlinkKit.find(params[:id])
+    starlink_kit.destroy
   end
 
   def check_kit_number
@@ -48,10 +49,6 @@ class Api::V1::StarlinkKitsController < ApplicationController
   
 
   private
-
-  def set_starlink_kit
-    @starlink_kit = StarlinkKit.find(params[:id])
-  end
 
   def starlink_kit_params
     params.require(:starlink_kit).permit(:kit_number, :address, :nin, :company_name, :company_number, :starlink_user_id)
