fea04ecda54b241e368b4de9be52210c522df199
geekelo <efueliht12@gmail.com>
2025-02-22 23:50:00 +0100

    Fix twilio

--- app/services/whatsapp_sender.rb
+++ app/services/whatsapp_sender.rb
@@ -1,17 +1,15 @@
-# app/services/whatsapp_sender.rb
 require 'twilio-ruby'
 
 class WhatsappSender
   def self.send_otp(to, otp)
     account_sid = ENV['TWILIO_ACCOUNT_SID']
     auth_token = ENV['TWILIO_AUTH_TOKEN']
-    client = Twilio::REST::Client.new(account_sid, auth_token)
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
 
-    message = client.messages.create(
+    message = @client.messages.create(
       from: 'whatsapp:+14155238886', # Twilio Sandbox Number
-      content_sid: ENV['TWILIO_CONTENT_SID'], # Pre-approved WhatsApp Template SID
-      content_variables: "{\"1\":\"#{otp}\"}", # Dynamic OTP in template
-      to: "whatsapp:#{to}"
+      to: "whatsapp:#{to}",
+      body: "Your OTP code is: #{otp}" # Send OTP as a simple message
     )
 
     message.sid
