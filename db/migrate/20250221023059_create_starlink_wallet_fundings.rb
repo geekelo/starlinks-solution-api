a08ee878928dfcdef7a52b773c7c08220a87fe9a
geekelo <efueliht12@gmail.com>
2025-02-22 10:03:06 +0100

    Fix bugs

--- db/migrate/20250221023059_create_starlink_wallet_fundings.rb
+++ db/migrate/20250221023059_create_starlink_wallet_fundings.rb
@@ -6,13 +6,15 @@ class CreateStarlinkWalletFundings < ActiveRecord::Migration[7.1]
       t.decimal :amount, precision: 15, scale: 2, null: false
       t.string :status, null: false, default: "pending"
       t.string :paid, null: false, default: "no"
-      t.string :reference, null: false, unique: true
+      t.string :reference, null: false
       t.string :payment_method, null: false # e.g., 'credit_card', 'bank_transfer', etc.
-      t.string :transaction_id, null: false, unique: true # Unique transaction ID for payment gateway
+      t.string :transaction_id, null: false
 
       t.timestamps
     end
 
+    # Add unique indexes separately
+    add_index :starlink_wallet_fundings, :reference, unique: true
     add_index :starlink_wallet_fundings, :transaction_id, unique: true
   end
 
@@ -20,3 +22,4 @@ class CreateStarlinkWalletFundings < ActiveRecord::Migration[7.1]
     drop_table :starlink_wallet_fundings
   end
 end
+
