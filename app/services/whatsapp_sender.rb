57200538820b38e758e1db2f7ae2e976c5202c88
geekelo <efueliht12@gmail.com>
2025-02-22 23:08:00 +0100

    Reverse changes made by rubocop on whatsapp sender

--- app/services/whatsapp_sender.rb
+++ app/services/whatsapp_sender.rb
@@ -3,13 +3,13 @@ require 'twilio-ruby'
 
 class WhatsappSender
   def self.send_otp(to, otp)
-    account_sid = ENV.fetch('TWILIO_ACCOUNT_SID')
-    auth_token = ENV.fetch('TWILIO_AUTH_TOKEN')
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
     client = Twilio::REST::Client.new(account_sid, auth_token)
 
     message = client.messages.create(
       from: 'whatsapp:+14155238886', # Twilio Sandbox Number
-      content_sid: ENV.fetch('TWILIO_CONTENT_SID'), # Pre-approved WhatsApp Template SID
+      content_sid: ENV['TWILIO_CONTENT_SID'], # Pre-approved WhatsApp Template SID
       content_variables: "{\"1\":\"#{otp}\"}", # Dynamic OTP in template
       to: "whatsapp:#{to}"
     )
