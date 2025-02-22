4e935fd94f299b6760c3ddae564c3ffd810671a9
geekelo <efueliht12@gmail.com>
2025-02-22 11:29:31 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_19_124238) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_22_095146) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_19_124238) do
     t.string "company_name"
     t.string "company_number"
     t.string "status", default: "pending", null: false
-    t.string "service_line_number"
+    t.string "service_line_number", null: false
     t.uuid "starlink_plan_id", null: false
     t.uuid "starlink_user_id", null: false
     t.datetime "created_at", null: false
@@ -37,6 +37,16 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_19_124238) do
     t.string "status", default: "optional", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "starlink_user_wallets", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "starlink_user_id", null: false
+    t.string "wallet_id", limit: 6, null: false
+    t.decimal "balance", precision: 15, scale: 2, default: "0.0", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["starlink_user_id"], name: "index_starlink_user_wallets_on_starlink_user_id"
+    t.index ["wallet_id"], name: "index_starlink_user_wallets_on_wallet_id", unique: true
   end
 
   create_table "starlink_users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -56,11 +66,34 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_19_124238) do
     t.datetime "confirmation_sent_at"
     t.string "whatsapp_confirmation_token"
     t.datetime "whatsapp_confirmation_sent_at"
+    t.uuid "starlink_kit_id"
     t.index ["confirmation_token"], name: "index_starlink_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_starlink_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_starlink_users_on_reset_password_token", unique: true
+    t.index ["starlink_kit_id"], name: "index_starlink_users_on_starlink_kit_id"
+  end
+
+  create_table "starlink_wallet_fundings", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "starlink_user_id", null: false
+    t.uuid "starlink_user_wallet_id", null: false
+    t.decimal "amount", precision: 15, scale: 2, null: false
+    t.string "status", default: "pending", null: false
+    t.string "paid", default: "no", null: false
+    t.string "reference", null: false
+    t.string "payment_method", null: false
+    t.string "transaction_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["reference"], name: "index_starlink_wallet_fundings_on_reference", unique: true
+    t.index ["starlink_user_id"], name: "index_starlink_wallet_fundings_on_starlink_user_id"
+    t.index ["starlink_user_wallet_id"], name: "index_starlink_wallet_fundings_on_starlink_user_wallet_id"
+    t.index ["transaction_id"], name: "index_starlink_wallet_fundings_on_transaction_id", unique: true
   end
 
   add_foreign_key "starlink_kits", "starlink_plans"
   add_foreign_key "starlink_kits", "starlink_users"
+  add_foreign_key "starlink_user_wallets", "starlink_users"
+  add_foreign_key "starlink_users", "starlink_kits"
+  add_foreign_key "starlink_wallet_fundings", "starlink_user_wallets"
+  add_foreign_key "starlink_wallet_fundings", "starlink_users"
 end
