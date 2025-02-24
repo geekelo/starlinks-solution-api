ffe2868f55ab197a59ae2b94708b9b1d574e6f01
geekelo <efueliht12@gmail.com>
2025-02-24 23:32:08 +0100

    Fix kit show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,3 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
-  attr_reader :current_user
-
-  private
-
-  def authenticate_user!
-    payload = JsonWebToken.decode(auth_token)
-    @current_user = StarlinkUser.find(payload['user_id'])
-  rescue JWT::DecodeError
-    render json: { error: 'Invalid token' }, status: :unauthorized
-  end
-
-  def auth_token
-    @auth_token ||= request.headers.fetch('authorization', '').split.last
-  end
 end
