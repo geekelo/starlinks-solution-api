4d9ac3418d4b5a025b07fe7aca961a9adc49e9f0
geekelo <efueliht12@gmail.com>
2025-02-25 22:05:08 +0100

    Fix 500 error with starlink kit fetch

--- app/models/starlink_kit.rb
+++ app/models/starlink_kit.rb
@@ -2,12 +2,25 @@
 class StarlinkKit < ApplicationRecord
   belongs_to :starlink_user, optional: true, dependent: :destroy
   belongs_to :starlink_plan, optional: true  
+  has_many :starlink_kit_renewals
 
   before_validation :set_default_plan, on: :create
 
   def self.kit_number_exists?(kit_number)
     exists?(kit_number: kit_number)
   end
+
+  def check_and_deactivate_kit(kit)
+    renewal = kit.starlink_kit_renewals
+                 .where(status: "invoice", paid: false)
+                 .order(due_date: :desc)
+                 .first
+  
+    if renewal && renewal.due_date < Date.today
+      kit.update!(status: 'deactivated')
+    end
+  end
+  
 
   private
 
