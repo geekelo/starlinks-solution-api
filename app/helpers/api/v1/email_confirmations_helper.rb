17110ddff9883d86050e884e60b56e6ead407a32
geekelo <efueliht12@gmail.com>
2025-02-26 01:08:45 +0100

    Troubleshoot

--- app/helpers/api/v1/email_confirmations_helper.rb
+++ app/helpers/api/v1/email_confirmations_helper.rb
@@ -1,16 +1,15 @@
 module Api::V1::EmailConfirmationsHelper
 
   def generate_confirmation_token
-    confirmation_token = SecureRandom.hex(20)
-    confirmation_sent_at = Time.current
+    token = SecureRandom.hex(20) # Generate the token first
+    self.update!(
+      confirmation_token: token,
+      confirmation_sent_at: Time.current
+    )
+    token
+  end
   
-    if save
-      confirmation_token
-    else
-      Rails.logger.error "Failed to save confirmation token: #{errors.full_messages}"
-    end
-  end  
-  
+
   def confirmation_token_valid?
     confirmation_sent_at && confirmation_sent_at >= 2.hours.ago
   end
