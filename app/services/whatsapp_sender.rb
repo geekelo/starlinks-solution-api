3f45bb4c854ea999f7438a432c17cd2320a25464
geekelo <efueliht12@gmail.com>
2025-03-03 22:16:21 +0100

    Switch from whatsapp to message

--- app/services/whatsapp_sender.rb
+++ app/services/whatsapp_sender.rb
@@ -5,15 +5,32 @@ class WhatsappSender
     account_sid = ENV['TWILIO_ACCOUNT_SID']
     auth_token = ENV['TWILIO_AUTH_TOKEN']
     @client = Twilio::REST::Client.new(account_sid, auth_token)
-
     message = @client.messages.create(
-      # from: 'whatsapp:+14155238886', # Twilio Sandbox Number
-      from: 'whatsapp-sender:+15557296949',
-      to: "whatsapp:#{to}",
-      body: "**Starlink Installation Solutions**: Enter this code **#{otp}** on our website to proceed.
-        Send **STOP** here on WhatsApp to complete the verification process."
+      body: 'STARLINK SOLUTIONS: Your OTP is ' + otp,
+      from: '+18573406807',
+      to: to,
     )
+    puts message.sid
 
-    message.sid
   end
 end
+
+
+
+# class WhatsappSender
+#   def self.send_otp(to, otp)
+#     account_sid = ENV['TWILIO_ACCOUNT_SID']
+#     auth_token = ENV['TWILIO_AUTH_TOKEN']
+#     @client = Twilio::REST::Client.new(account_sid, auth_token)
+
+#     message = @client.messages.create(
+#       # from: 'whatsapp:+14155238886', # Twilio Sandbox Number
+#       from: 'whatsapp:+15557296949',
+#       to: "whatsapp:#{to}",
+#       body: "**Starlink Installation Solutions**: Enter this code **#{otp}** on our website to proceed.
+#         Send **STOP** here on WhatsApp to complete the verification process."
+#     )
+
+#     message.sid
+#   end
+# end
