d51dfb1f95b913509859bd9576a48f9524877ebb
geekelo <efueliht12@gmail.com>
2025-03-05 22:39:42 +0100

    Improve email display

--- app/mailers/confirmation_email_mailer.rb
+++ app/mailers/confirmation_email_mailer.rb
@@ -4,6 +4,6 @@ class ConfirmationEmailMailer < ApplicationMailer
   def send_confirmation_email(user, token)
     @user = user
     @token = token
-    mail(to: @user.email, subject: 'Confirm Your Password')
+    mail(to: @user.email, subject: 'Confirm Your Password - Starlink Solutions')
   end
 end
--- app/mailers/password_reset_mailer.rb
+++ app/mailers/password_reset_mailer.rb
@@ -4,6 +4,6 @@ class PasswordResetMailer < ApplicationMailer
   def send_reset_email(user, token)
     @user = user
     @token = token
-    mail(to: @user.email, subject: 'Reset Your Password')
+    mail(to: @user.email, subject: 'Reset Your Password - Starlink Solutions')
   end
 end
