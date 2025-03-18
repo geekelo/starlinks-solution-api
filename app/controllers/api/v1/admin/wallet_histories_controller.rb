c55a5da47f992947f3e28a231c9b7d8c91219cd0
geekelo <efueliht12@gmail.com>
2025-03-18 22:44:19 +0100

    Improve query for fundings

--- app/controllers/api/v1/admin/wallet_histories_controller.rb
+++ app/controllers/api/v1/admin/wallet_histories_controller.rb
@@ -4,10 +4,15 @@ class Api::V1::Admin::WalletHistoriesController < ApplicationController
   # GET /api/v1/admin/wallet_history
   def index
     wallet_history = {
-      fundings: StarlinkWalletFunding.where(status: 'approved')
-                               .joins(:starlink_user_wallet)
-                               .select('starlink_fundings.id, starlink_fundings.amount, starlink_fundings.funding_date, starlink_fundings.source, starlink_fundings.created_at, starlink_user_wallets.user_email'),
-      
+      fundings = StarlinkWalletFunding.where(status: 'approved')
+      .joins(starlink_user_wallet: :starlink_user) # Ensure proper join path
+      .select('starlink_wallet_fundings.id, 
+               starlink_wallet_fundings.amount, 
+               starlink_wallet_fundings.funding_date, 
+               starlink_wallet_fundings.source, 
+               starlink_wallet_fundings.created_at, 
+               starlink_users.email') # Reference email correctly
+
       renewals: StarlinkKitRenewal.where(status: 'receipt')
                                   .joins(starlink_kit: :starlink_user)
                                   .select('starlink_kit_renewals.id, starlink_kit_renewals.amount, starlink_kit_renewals.date_of_renewal, starlink_kit_renewals.created_at, starlink_kits.kit_number, starlink_users.email AS user_email'),
@@ -19,4 +24,6 @@ class Api::V1::Admin::WalletHistoriesController < ApplicationController
   rescue StandardError => e
     render json: { error: e.message }, status: :internal_server_error
   end
+
+
 end
