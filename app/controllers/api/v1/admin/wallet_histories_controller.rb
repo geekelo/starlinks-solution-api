9f962224783936509086468257fd9363470b6497
geekelo <efueliht12@gmail.com>
2025-03-18 23:52:43 +0100

    Improve code

--- app/controllers/api/v1/admin/wallet_histories_controller.rb
+++ app/controllers/api/v1/admin/wallet_histories_controller.rb
@@ -5,22 +5,20 @@ class Api::V1::Admin::WalletHistoriesController < ApplicationController
   def index
     wallet_history = {
       fundings: StarlinkWalletFunding.where(status: 'approved')
-                                     .joins(starlink_user_wallet: :starlink_user)
                                      .select('starlink_wallet_fundings.id, 
                                               starlink_wallet_fundings.amount, 
-                                              starlink_wallet_fundings.funding_date, 
-                                              starlink_wallet_fundings.source, 
+                                              starlink_wallet_fundings.reference, 
+                                              starlink_wallet_fundings.status, 
                                               starlink_wallet_fundings.created_at, 
-                                              starlink_users.email'),
+                                              starlink_wallet_fundings.starlink_users.email'),
 
       renewals: StarlinkKitRenewal.where(status: 'receipt')
-                                  .joins(starlink_kit: :starlink_user)
                                   .select('starlink_kit_renewals.id, 
                                            starlink_kit_renewals.amount, 
                                            starlink_kit_renewals.date_of_renewal, 
                                            starlink_kit_renewals.created_at, 
-                                           starlink_kits.kit_number, 
-                                           starlink_users.email AS user_email'),
+                                           starlink_kit_renewals.starlink_kits.kit_number, 
+                                           starlink_kit_renewals.starlink_users.email AS user_email'),
 
       withdrawals: StarlinkAdminWithdrawal.select(:id, :amount, :purpose, :withdrawal_date, :created_at)
     }
