456c46627e1ab27167409ddc58fbbc6872aa2620
geekelo <efueliht12@gmail.com>
2025-04-09 11:35:00 +0100

    Fix invoice reminders

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -3,7 +3,7 @@ class InvoiceMailer < ApplicationMailer
 
   def reminder_email(invoice)
     @invoice = invoice
-    @user = invoice.user
+    @user = invoice.starlink_user
     mail(to: @user.email, subject: "Invoice Due Soon: ##{@invoice.id}")
   end
 end
