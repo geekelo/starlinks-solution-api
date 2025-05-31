a332794ba6a711e8917ec772194e63acacd20195
Eloghene <eloghene@bananacrystal.com>
2025-05-31 20:26:53 +0100

    Fix invoice creation after paying for prorated

--- app/models/starlink_kit_renewal.rb
+++ app/models/starlink_kit_renewal.rb
@@ -15,9 +15,9 @@ class StarlinkKitRenewal < ApplicationRecord
                          .first
     
     plan_price = kit_plan&.price || 0
+   
     # if payment is done late (after end date) or a fresh user
-    if last_renewal.nil? || last_renewal&.end_date < Date.today
-  
+    if last_renewal.nil?
       # Receipt for the current month
       kit.starlink_kit_renewals.create!(
         starlink_kit_id: kit_id,
@@ -51,7 +51,40 @@ class StarlinkKitRenewal < ApplicationRecord
         starlink_user_wallet_id: wallet.id,
         prorated: true
       )
+     elsif last_renewal&.prorated
+      kit.starlink_kit_renewals.create!(
+        starlink_kit_id: kit_id,
+        amount: plan_price,
+        start_date: last_renewal.end_date >= Date.today ? last_renewal.end_date + 1.day : Date.today,
+        deadline: last_renewal.end_date >= Date.today ? last_renewal.end_date : Date.today,
+        end_date: last_renewal.end_date >= Date.today ? last_renewal.end_date + 32.days : (Date.today.next_month - 1.day),
+        status: "invoice",
+        month: last_renewal.end_date >= Date.today ? Date.today.next_month.month : Date.today.month,
+        year: last_renewal.end_date >= Date.today ? next_month.year : Date.today.year,
+        starlink_user_id: kit.starlink_user_id,
+        starlink_user_wallet_id: wallet.id
+      )
 
+      if last_renewal.end_date < Date.today
+        # Prorated Invoice
+        next_month = Date.today.next_month
+        days_remaining = (31 - Date.today.day)
+        invoice_amount = days_remaining * 4000
+    
+        kit.starlink_kit_renewals.create!(
+          starlink_kit_id: kit_id,
+          amount: invoice_amount,
+          deadline: (Date.today.next_month - 1.day),
+          start_date: Date.today.next_month,
+          end_date: next_month.end_of_month,
+          status: "invoice",
+          month: next_month.month,
+          prorated: true,
+          year: next_month.year,
+          starlink_user_id: kit.starlink_user_id,
+          starlink_user_wallet_id: wallet.id
+        )
+      end
     else
       # Invoice for next month
       next_month = last_renewal.end_date.next_month
@@ -101,7 +134,7 @@ class StarlinkKitRenewal < ApplicationRecord
       unpaid_renewals_sum
     elsif last_invoice&.prorated && last_invoice.end_date <= Date.today
       # If the last invoice's end date is today or before today, add the plan price
-      unpaid_renewals_sum + plan_price
+      unpaid_renewals_sum
     else
       unpaid_renewals_sum
     end
