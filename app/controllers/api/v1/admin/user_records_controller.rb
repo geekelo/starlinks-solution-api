cf41c912d9c66c7a5af1f3bd959a148e55e33f7b
Eloghene <eloghene@bananacrystal.com>
2025-09-20 15:48:30 +0100

    Add pagination to admin users

--- app/controllers/api/v1/admin/user_records_controller.rb
+++ app/controllers/api/v1/admin/user_records_controller.rb
@@ -3,7 +3,21 @@ class Api::V1::Admin::UserRecordsController < ApplicationController
 
   # GET /api/v1/admin/user_records
   def index
-    user_records = StarlinkUser.where(role: 'user').includes(:starlink_kits)
+    user_records = StarlinkUser.where(role: 'user').includes(:starlink_kits, :starlink_user_wallet)
+    user_records = user_records.order(created_at: :desc)
+
+    # filter by name, email, phone_number, whatsapp_number, wallet_id
+    filterName = params[:filter][:name] if params[:filter][:name].present?
+    filterEmail = params[:filter][:email] if params[:filter][:email].present?
+    filterPhoneNumber = params[:filter][:phone_number] if params[:filter][:phone_number].present?
+    filterWhatsappNumber = params[:filter][:whatsapp_number] if params[:filter][:whatsapp_number].present?
+    filterWalletId = params[:filter][:wallet_id] if params[:filter][:wallet_id].present?
+
+    user_records = user_records.where("name ILIKE ?", "%#{filterName}%") if filterName.present?
+    user_records = user_records.where("email ILIKE ?", "%#{filterEmail}%") if filterEmail.present?
+    user_records = user_records.where("phone_number ILIKE ?", "%#{filterPhoneNumber}%") if filterPhoneNumber.present?
+    user_records = user_records.where("whatsapp_number ILIKE ?", "%#{filterWhatsappNumber}%") if filterWhatsappNumber.present?
+    user_records = user_records.joins(:starlink_user_wallet).where(starlink_user_wallets: { wallet_id: filterWalletId }) if filterWalletId.present?
 
     user_data = user_records.map do |user|
       {
@@ -22,7 +36,10 @@ class Api::V1::Admin::UserRecordsController < ApplicationController
       }
     end
 
-    render json: user_data, status: :ok
+    # pagination
+    user_records = user_records.paginate(page: params[:page], per_page: params[:per_page] || 50)
+
+    render json: { user_data: user_data, total_pages: user_records.total_pages, total_count: user_records.total_count }, status: :ok
   rescue StandardError => e
     render json: { error: e.message }, status: :internal_server_error
   end
